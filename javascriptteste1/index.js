598849e825a6f26ac4eb5f54edf58bacb267767d
Otavio Henrique <OtavioHenLimSil@gmail.com>
2020-08-18 22:22:22 -0300

    Commit 21 dos arquivos

--- javascriptteste1/index.js
+++ javascriptteste1/index.js
@@ -45,28 +45,29 @@ const teorema_de_pitagoras = (cateto1, cateto2) => Math.sqrt((cateto1**2) + (cat
 // -------------------------------------------------------
 // -----------T-34/76-----------
 // console.log((200*2)/165);
-const calculaMedida1 = (x) => {
+const calculaMedida_T34_76 = (x) => {
     let r1 = (200*x)/165;
     let r2 = `${x} -> ${r1.toFixed(2)} -> ${Math.round(r1)}`;
     return r2;
 };
-// console.log(calculaMedida1(4));
+// console.log(calculaMedida_T34_76(0.5));
 // console.log(teorema_de_pitagoras(11, 19));
 // console.log((21*7)/12);
 // console.log((165*20)/260);
-const calculaTamanhoRoda2 = (x) => {
+const calculaTamanhoRoda2_T34_76 = (x) => {
     let r1 = (24*x)/44;
     let r2 = (200*r1)/165;
     let r3 = `Roda 2 -> ${x} -> ${r1.toFixed(2)} -> ${r2.toFixed(2)} -> ${Math.round(r2)}`;
     // let r3 = `Roda 2 -> ${r1.toFixed(2)} -> ${Math.round(r1)} -> ${r2.toFixed(2)} -> ${Math.round(r2)}`;
     return r3;
 };
-// console.log(calculaTamanhoRoda2(1)); // Roda 2
-const calculaTamanhoRoda2_2 = (x) => {
+// console.log(calculaTamanhoRoda2_T34_76(1));
+const calculaTamanhoChassi_T34_76 = (x) => {
+    /* Imagem de cima */
     let r1 = (165*x)/165;
     let r2 = (200*r1)/165;
-    let r3 = `Roda 2 -> ${x} -> ${r1.toFixed(2)} -> ${r2.toFixed(2)} -> ${Math.round(r2)}`;
+    let r3 = `Chassi -> ${x} -> ${r1.toFixed(2)} -> ${r2.toFixed(2)} -> ${Math.round(r2)}`;
     return r3;
 };
-console.log(calculaTamanhoRoda2_2(15));
+console.log(calculaTamanhoChassi_T34_76(6.5));
 // -------------------------------------------------------
