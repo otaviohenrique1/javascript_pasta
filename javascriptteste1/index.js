346d610e13c8d5d86d772bef7ab420eb203ae33e
Otavio Henrique <OtavioHenLimSil@gmail.com>
2020-08-25 23:07:55 -0300

    Commit 24 dos arquivos

--- javascriptteste1/index.js
+++ javascriptteste1/index.js
@@ -5,10 +5,19 @@ const teorema_de_pitagoras = (cateto1, cateto2) => Math.sqrt((cateto1**2) + (cat
 // -----------Navio -> Nelson-class battleship-----------
 // a-b
 // c-x
-// console.log('Comprimento ->' ,(760*1)/280);
-// console.log('Altura -> ', (277*1)/91);
+// console.log('Comprimento ->' ,(760*7)/280);
+// console.log('Altura -> ', (277*4)/91);
 // console.log('Largura -> ', (120*5)/43);
 // console.log((280*1)/350);
+// console.log((128*280)/43);
+// console.log((280*19)/834);
+const calculaMedida_Bateria_8_Canhoes = (x) => {
+    let r1 = (7*x)/50;
+    let r2 = (19*r1)/7;
+    let r3 = `Bateria 8 canhoes -> ${x} -> ${r1.toFixed(2)} -> ${r2.toFixed(2)}`;
+    return r3;
+};
+console.log(calculaMedida_Bateria_8_Canhoes(15));
 // console.log(`Comprimento -> ${calculaMedida(760, 4.5, 280)}`);
 // console.log(`Altura -> ${calculaMedida(277, 2.5, 91)}`);
 // console.log(`Largura -> ${calculaMedida(120, 2.5, 43)}`);
@@ -90,7 +99,7 @@ const calculaTamanhoChassi_2_T34_76 = (x) => {
 // console.log(calculaTamanhoChassi_2_T34_76(5));
 // console.log(calculaTamanhoChassi_T34_76(14));
 // console.log(calculaTamanhoRoda1_T34_76(6));
-console.log(calculaTamanhoRoda2_T34_76(3));
+// console.log(calculaTamanhoRoda2_T34_76(3));
 // console.log(calculaTamanhoRoda3_T34_76(6));
 
 
