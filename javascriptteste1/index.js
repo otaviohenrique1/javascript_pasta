9596db4f7b937b95c25212139942c0a098ae7393
Otavio Henrique <OtavioHenLimSil@gmail.com>
2020-09-09 01:35:10 -0300

    commit 10 dos arquivos

--- javascriptteste1/index.js
+++ javascriptteste1/index.js
@@ -14,10 +14,10 @@ const teorema_de_pitagoras = (cateto1, cateto2) => Math.sqrt((cateto1**2) + (cat
 const calculaMedida_Bateria_8_Canhoes = (x) => {
     let r1 = (7*x)/50;
     let r2 = (19*r1)/7;
-    let r3 = `Bateria 8 canhoes -> ${x} -> ${r1.toFixed(2)} -> ${r2.toFixed(2)}`;
+    let r3 = `Bateria 8 canhoes -> ${x} -> ${r1.toFixed(2)} -> ${r2.toFixed(2)} -> ${Math.round(r2)}`;
     return r3;
 };
-console.log(calculaMedida_Bateria_8_Canhoes(15));
+// console.log(calculaMedida_Bateria_8_Canhoes(15));
 // console.log(`Comprimento -> ${calculaMedida(760, 4.5, 280)}`);
 // console.log(`Altura -> ${calculaMedida(277, 2.5, 91)}`);
 // console.log(`Largura -> ${calculaMedida(120, 2.5, 43)}`);
@@ -101,6 +101,85 @@ const calculaTamanhoChassi_2_T34_76 = (x) => {
 // console.log(calculaTamanhoRoda1_T34_76(6));
 // console.log(calculaTamanhoRoda2_T34_76(3));
 // console.log(calculaTamanhoRoda3_T34_76(6));
-
-
 // -------------------------------------------------------
+// -----------Roberts-class monitor-HMS Roberts-----------
+const calculaMedidaHMSRoberts1 = (valor) => {
+    let r1 = (600*valor)/269;
+    let r2 = `${valor} -> ${r1.toFixed(2)} -> ${Math.round(r1)}`;
+    return r2;
+};
+const calculaMedidaHMSRoberts2 = (texto, valor) => {
+    let r1 = (600*valor)/269;
+    let r2 = `${texto} -> ${valor} -> ${r1.toFixed(2)} -> ${Math.round(r1)}`;
+    return r2;
+};
+const calculaMedidaHMSRoberts3 = (texto, valor) => {
+        let r1 = (600*valor)/270;
+    let r2 = `${texto} -> ${valor} -> ${r1.toFixed(2)} -> ${Math.round(r1)}`;
+    return r2;
+};
+const calculaMedidaHMSRoberts4 = (valor) => {
+    let r1 = (700*valor)/269;
+    let r2 = `${valor} -> ${r1.toFixed(2)} -> ${Math.round(r1)}`;
+    return r2;
+};
+const calculaMedidaHMSRoberts5 = (valor) => {
+    let r1 = (600*valor)/264;
+    let r2 = `${valor} -> ${r1.toFixed(2)} -> ${Math.round(r1)}`;
+    return r2;
+};
+// console.log(calculaMedidaHMSRoberts1(42));
+// console.log(calculaMedidaHMSRoberts2('C02', 7));
+// console.log(calculaMedidaHMSRoberts3('C02', 29));
+// console.log(calculaMedidaHMSRoberts4(10));
+// console.log(calculaMedidaHMSRoberts5(10));
+// -------------------------------------------------------
+console.log(`C01 -> 264 -> 600 -> 600`);
+console.log(`C02 -> ${calculaMedidaHMSRoberts5(1)}`);
+console.log(`C03 -> ${calculaMedidaHMSRoberts5(9.5)}`);
+console.log(`C04 -> ${calculaMedidaHMSRoberts5(23.5)}`);
+console.log(`C05 -> ${calculaMedidaHMSRoberts5(23)}`);
+console.log(`C06 -> ${calculaMedidaHMSRoberts5(23)}`);
+console.log(`C07 -> ${calculaMedidaHMSRoberts5(23.5)}`);
+console.log(`C08 -> ${calculaMedidaHMSRoberts5(23)}`);
+console.log(`C09 -> ${calculaMedidaHMSRoberts5(23)}`);
+console.log(`C10 -> ${calculaMedidaHMSRoberts5(23)}`);
+console.log(`C11 -> ${calculaMedidaHMSRoberts5(23)}`);
+console.log(`C12 -> ${calculaMedidaHMSRoberts5(23.5)}`);
+console.log(`C13 -> ${calculaMedidaHMSRoberts5(14.5)}`);
+console.log(`C14 -> ${calculaMedidaHMSRoberts5(8.5)}`);
+console.log(`C15 -> ${calculaMedidaHMSRoberts5(12)}`);
+console.log(`C16 -> ${calculaMedidaHMSRoberts5(11)}`);
+console.log(`C17 -> ${calculaMedidaHMSRoberts5(23)}`);
+console.log(`C18 -> ${calculaMedidaHMSRoberts5(22.5)}`);
+console.log(`C19 -> ${calculaMedidaHMSRoberts5(22)}`);
+console.log(`C20 -> ${calculaMedidaHMSRoberts5(21)}`);
+console.log(`C21 -> ${calculaMedidaHMSRoberts5(20.5)}`);
+console.log(`C22 -> ${calculaMedidaHMSRoberts5(20)}`);
+console.log(`C23 -> ${calculaMedidaHMSRoberts5(20)}`);
+console.log(`C24 -> ${calculaMedidaHMSRoberts5(20)}`);
+console.log(`C25 -> ${calculaMedidaHMSRoberts5(20)}`);
+console.log(`C26 -> ${calculaMedidaHMSRoberts5(20)}`);
+console.log(`C27 -> ${calculaMedidaHMSRoberts5(20)}`);
+console.log(`C28 -> ${calculaMedidaHMSRoberts5(20)}`);
+console.log(`C29 -> ${calculaMedidaHMSRoberts5(17)}`);
+console.log(`C30 -> ${calculaMedidaHMSRoberts5(14)}`);
+console.log(`C31 -> ${calculaMedidaHMSRoberts5(9)}`);
+console.log(`C32 -> ${calculaMedidaHMSRoberts5(5)}`);
+console.log(`C33 -> ${calculaMedidaHMSRoberts5(3.5)}`);
+console.log(`C34 -> ${calculaMedidaHMSRoberts5(5)}`);
+console.log(`C35 -> ${calculaMedidaHMSRoberts5(4.5)}`);
+console.log(`C36 -> ${calculaMedidaHMSRoberts5(5)}`);
+console.log(`C37 -> ${calculaMedidaHMSRoberts5(10)}`);
+console.log(`C38 -> ${calculaMedidaHMSRoberts5(7)}`);
+console.log(`C39 -> ${calculaMedidaHMSRoberts5(4)}`);
+console.log(`C40 -> ${calculaMedidaHMSRoberts5(64)}`);
+console.log(`C41 -> ${calculaMedidaHMSRoberts5(56)}`);
+console.log(`C42 -> ${calculaMedidaHMSRoberts5(42)}`);
+console.log(`C43 -> ${calculaMedidaHMSRoberts5(7)}`);
+console.log(`C44 -> ${calculaMedidaHMSRoberts5(5)}`);
+console.log(`C45 -> ${calculaMedidaHMSRoberts5(7)}`);
+console.log(`C46 -> ${calculaMedidaHMSRoberts5(8)}`);
+console.log(`C47 -> ${calculaMedidaHMSRoberts5(9)}`);
+console.log(`C48 -> ${calculaMedidaHMSRoberts5(5)}`);
+console.log(`C49 -> ${calculaMedidaHMSRoberts5(8)}`);
